7b65593f1bc016b93dc5303f3c13969df7cdbca6
MartG-09 <ajulogbemi09@gmail.com>
2026-08-14 22:27:35 +0100

    finish

---
+++ CHAPTER ONE.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE users_db;
+
+CREATE TABLE users (
+	id VARCHAR(50) PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    phone_number VARCHAR(20)
+);
+
+SHOW TABLES;
+
+
+CREATE DATABASE products_db;
+
+
+
+
+
+
+CREATE DATABASE e_commerce_db;
+
+CREATE TABLE users (
+	id VARCHAR(50) PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    phone_number VARCHAR(20)
+);
+
+
+CREATE TABLE products (
+	id VARCHAR(50) NOT NULL,
+    product_name VARCHAR(50) NOT NULL,
+    brand VARCHAR(50) NOT NULL,
+    selling_price VARCHAR(50) NOT NULL,
+    quantity VARCHAR(50) NOT NULL,
+    expiry_date VARCHAR(50)
+    
+);
+    
+CREATE TABLE 
+    
+
+
+
+
+	
+    
---
+++ users.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE dikedb;
+
+CREATE TABLE users (
+	id VARCHAR(50) PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    phone_number VARCHAR(20)
+);
+
+SHOW TABLES;
+	
+    
